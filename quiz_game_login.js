fc0775fa19c8edf3168657fb06a93ec4544ed101
Abhiveer773 <90145630+Abhiveer773@users.noreply.github.com>
2022-03-11 23:27:43 +0530

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,10 @@
+function send()
+{
+    player1_input = document.getElementById("player1_input").value;
+    player2_input = document.getElementById("player2_input").value;
+
+    localStorage.setItem ("player1_input", player1_input);
+    localStorage.setItem ("player_2input", player2_input);
+  
+    window.location = "quiz_game_page.html";
+}
